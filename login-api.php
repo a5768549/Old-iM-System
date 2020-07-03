5e4a15be9f720d157c0161f6e11fe4db09cfd2ef
a5768549 <44581221+a5768549@users.noreply.github.com>
2020-07-03 13:13:29 +0800

    add photo request

--- login-api.php
+++ login-api.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 session_start();
 header("Content-Type: application/json; charset=utf8");
@@ -35,8 +35,37 @@ if ($is_login_info_valid) {
 
     if ($is_password_verified) {
         $_SESSION["login"] = $name;
+        
+        $special_Account = ["C2PAFF","C2NEKO","C2ROBO","C2Ivy","C2Xenon","C2ConneR","C2Cherry","C2JOE","C2Nora"];
+        $special_color = [  "#59BD9C",
+                            "#D693B5",
+                            "#82B4C5",
+                            "#55555D",
+                            "#BB4646",
+                            "#9F6932",
+                            "#7E434F",
+                            "#623873",
+                            "#82878D"];
+
+        $statement = $con->prepare("SELECT photo FROM member WHERE account = ?");
+        $statement->execute([$name]);
+
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $return_color = "#FFFFFF";
+
+        for($i=0;$i<9;$i++)
+        {
+            if($name == $special_Account[$i])
+            {
+                $return_color = $special_color[$i];
+            }
+        }
+        
         http_response_code(200);
-        exit(json_encode(["message" => "登入成功"]));
+        exit(json_encode(["message" => "登入成功",
+                          "photo_path" => $result["photo"],
+                          "border_color" => $return_color]));
     }
     else {
         http_response_code(401);
