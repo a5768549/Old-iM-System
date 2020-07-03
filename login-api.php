db6b52c702d871fd252de35278d9bad39e9f9619
fixiabis <fixiabis@gmail.com>
2020-07-03 09:59:33 +0800

    prevent other method request

--- login-api.php
+++ login-api.php
@@ -1,14 +1,23 @@
 <?php
 
 session_start();
-include('connect.php'); // 連接資料庫
 header("Content-Type: application/json; charset=utf8");
 
+/** 請求的方法是否允許 */
+$is_request_method_not_allowed = $_SERVER["REQUEST_METHOD"] !== "POST";
+
+if ($_is_request_method_not_allowed) {
+    http_response_code(405);
+    exit(json_encode(["message" => "不允許的方法"]));
+}
+
+include("connect.php"); // 連接資料庫
+
 /** 使用者名稱 */
-$name = htmlspecialchars($_POST['name']);
+$name = htmlspecialchars($_POST["name"]);
 
 /** 使用者密碼 */
-$password = htmlspecialchars($_POST['password']);
+$password = htmlspecialchars($_POST["password"]);
 
 /** 使用者登入資訊是否無誤 */
 $is_login_info_valid = $name && $password;
@@ -25,7 +34,7 @@ if ($is_login_info_valid) {
     $is_password_verified = password_verify($password, $member["password"]);
 
 	if ($is_password_verified) {
-        $_SESSION['login'] = $name;
+        $_SESSION["login"] = $name;
         http_response_code(200);
         exit(json_encode(["message" => "登入成功"]));
 	}
